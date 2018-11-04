43b39e6e927fa0cce1b982589439a67ad6e8ad47
Levarrishawk <levarrishawk@gmail.com>
2018-11-04 01:57:28 -0500

    [patch] 1.2.04a - Three Month Celebration

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -44,7 +44,7 @@
 --GENERAL PLAYER SETTINGS
 -----------------------------------
 
-onlineCharactersPerAccount = 3 --How many characters are allowed online from a single account.
+onlineCharactersPerAccount = 6 --How many characters are allowed online from a single account.
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
@@ -54,10 +54,10 @@ performanceDuration = 7200 -- in seconds
 medicalDuration = 7200 -- in seconds
 
 --Sets the experience multiplier while grouped
-groupExpMultiplier = 2.0
+groupExpMultiplier = 4.0 --2.0 default
 
 --Sets a global experience multiplier
-globalExpMultiplier = 1.0
+globalExpMultiplier = 3.0  -- 1.0 default
 
 --Sets the base number of control devices of each type that a player can have in their datapad at once
 --For creature pets, A Creature Handler will have the base number + their stored pets skill mod as limit
