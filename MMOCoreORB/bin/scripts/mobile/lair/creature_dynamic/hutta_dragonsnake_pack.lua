3dcbe2b776127482e33bd3b568b8aefeba0a5f98
Levarris <levarrishawk@gmail.com>
2018-11-15 21:27:12 -0500

    [hotfix] 1.4.03 - Removed lair references from Hutta dynamic spawn packs

--- MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/hutta_dragonsnake_pack.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/hutta_dragonsnake_pack.lua
@@ -1,11 +1,11 @@
 hutta_dragonsnake_pack = Lair:new {
 	mobiles = {{"dragonsnake",2},{"dragonsnake",1}},
 	spawnLimit = 35,
-	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsVeryEasy = {},
+  buildingsEasy = {},
+  buildingsMedium = {},
+  buildingsHard = {},
+  buildingsVeryHard = {},
 	buildingType = "none"
 }
 
--- MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/hutta_xuuva_pack.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/hutta_xuuva_pack.lua
@@ -1,11 +1,11 @@
 hutta_xuuva_pack = Lair:new {
 	mobiles = {{"xuuva",2},{"xuuva",1}},
 	spawnLimit = 35,
-	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
-	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsVeryEasy = {},
+  buildingsEasy = {},
+  buildingsMedium = {},
+  buildingsHard = {},
+  buildingsVeryHard = {},
 	buildingType = "none"
 }
 
