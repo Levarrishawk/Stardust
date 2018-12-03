9cfbdbef55cb16df456a97e235af061ddba00d9e
Levarrishawk <levarrishawk@gmail.com>
2018-12-03 04:32:59 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/object/tangible/terminal/terminal_event_buffs.lua
+++ MMOCoreORB/bin/scripts/object/tangible/terminal/terminal_event_buffs.lua
@@ -42,7 +42,7 @@
 
 
 object_tangible_terminal_terminal_event_buffs = object_tangible_terminal_shared_terminal_event_buffs:new {
-	customName = "Buffs",
+	customName = "Medical Enhancement Droid",
 	objectMenuComponent = "buffTerminalMenuComponent",
 }
 
