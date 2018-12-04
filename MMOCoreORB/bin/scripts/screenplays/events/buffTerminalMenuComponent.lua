64726cb6476291120f4b077030e108e74297a398
Levarrishawk <levarrishawk@gmail.com>
2018-12-04 02:19:13 -0500

    [iteration] Buff Droid

--- MMOCoreORB/bin/scripts/screenplays/events/buffTerminalMenuComponent.lua
+++ MMOCoreORB/bin/scripts/screenplays/events/buffTerminalMenuComponent.lua
@@ -5,6 +5,7 @@ buffTerminalMenuComponent = {  }
 function buffTerminalMenuComponent:fillObjectMenuResponse(pSceneObject, pMenuResponse, pPlayer)
 	local response = LuaObjectMenuResponse(pMenuResponse)
 	response:addRadialMenuItem(20, 3, "Get Buffs")
+	response:addRadialMenuItem(21, 3, "Clear Wounds")
 	
 end
 
@@ -27,6 +28,17 @@ function buffTerminalMenuComponent:handleObjectMenuSelect(pSceneObject, pPlayer,
 
     sui.sendTo(pPlayer)   	  	
 	
+	elseif selectedID == 21 then
+	
+	  local sui = SuiMessageBox.new("buffTerminalMenuComponent", "handleWoundChoice")
+    sui.setPrompt("It will cost 25000 Credits to heal your wounds. This is generally provided much cheaper by Entertainers.  Do you wish to proceed?")
+    sui.setTitle("Medical Enhancement Droid")
+    sui.setCancelButtonText("Cancel") -- Cancel  
+    sui.setOkButtonText("OK") -- OK   
+
+    sui.sendTo(pPlayer)    
+	
+    
 	end
 
 	return 0
@@ -49,4 +61,29 @@ function buffTerminalMenuComponent:handleBuffChoice(pPlayer, pSui, eventIndex, .
     CreatureObject(pPlayer):subtractCashCredits(5000)
     CreatureObject(pPlayer):sendSystemMessage("You successfully make a payment of 5000 credits.")
   end
+end
+
+function buffTerminalMenuComponent:handleWoundChoice(pPlayer, pSui, eventIndex, ...)
+
+  if (pPlayer == nil) then
+    return
+  end
+
+  local cancelPressed = (eventIndex == 1)
+  local args = {...}
+  
+  if (cancelPressed) then   
+    return 
+  elseif (eventIndex == 0) then -- Heal Wounds
+    
+    for i = 0, 8 do
+      CreatureObject(pPlayer):setWounds(i, 0)
+      
+    end
+    CreatureObject(pPlayer):playMusicMessage("sound/vo_meddroid_01.snd")
+    CreatureObject(pPlayer):subtractCashCredits(25000)
+    CreatureObject(pPlayer):sendSystemMessage("You successfully make a payment of 25000 credits.")
+  end
+
+
 end
