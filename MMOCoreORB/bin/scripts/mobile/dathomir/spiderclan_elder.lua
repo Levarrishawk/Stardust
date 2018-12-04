ed9a641a7d1da6d7130afbe18f573b3982b67f46
Levarrishawk <levarrishawk@gmail.com>
2018-12-04 04:23:43 -0500

    [hotfix] 1.5.13 - Rebalanced Damage of Several Dathomir Mobs

--- MMOCoreORB/bin/scripts/mobile/dathomir/spiderclan_elder.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/spiderclan_elder.lua
@@ -6,11 +6,11 @@ spiderclan_elder = Creature:new {
 	faction = "spider_nightsister",
 	level = 331, -- Should drop Flawless Crystals
 	chanceHit = 15.25,
-	damageMin = 1120,
-	damageMax = 1950,
+	damageMin = 120,
+	damageMax = 950,
 	baseXp = 26555,
-	baseHAM = 321000,
-	baseHAMmax = 392000,
+	baseHAM = 71000,
+	baseHAMmax = 92000,
 	armor = 1,
 	-- {kinetic,energy,blast,heat,cold,electricity,acid,stun,ls}
 	resists = {65,25,25,75,75,70,55,70,-1},
@@ -58,7 +58,7 @@ spiderclan_elder = Creature:new {
 				{group = "armor_attachments", chance = 2000000},      -- 50% * 20% = 10%
 				{group = "wearables_rare", chance = 2000000},
 				{group = "wearables_scarce", chance = 1000000},
-				{group = "vehicle_house_group", chance = 1000000},    -- 50% * 10% = 5%
+			--	{group = "vehicle_house_group", chance = 1000000},    -- 50% * 10% = 5%
 			},
 			lootChance = 5000000,    -- 50% chance for this loot group
 		},		
