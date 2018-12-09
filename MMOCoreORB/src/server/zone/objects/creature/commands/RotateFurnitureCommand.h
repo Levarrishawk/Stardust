a0dc08f80d2e22367aa3996684d37616ce420491
Levarris <levarrishawk@gmail.com>
2018-12-09 03:20:54 -0500

    [iteration] Y/P/R Rotation

--- MMOCoreORB/src/server/zone/objects/creature/commands/RotateFurnitureCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RotateFurnitureCommand.h
@@ -33,40 +33,63 @@ public:
 		int degrees = 0;
 
 		try {
-						UnicodeTokenizer tokenizer(arguments.toString());
-						tokenizer.getStringToken(dir);
-						degrees = tokenizer.getIntToken();
+					UnicodeTokenizer tokenizer(arguments.toString());
+					tokenizer.getStringToken(dir);
+					degrees = tokenizer.getIntToken();
 
-						dir = dir.toLowerCase();
+					dir = dir.toLowerCase();
 
-						if  (dir != "left" && dir != "right" && dir != "yaw" && dir != "roll" && dir != "pitch" && dir != "reset")
-							throw Exception();
+					if  (dir != "left" && dir != "right" && dir != "yaw" && dir != "roll" && dir != "pitch" && dir != "reset")
+						throw Exception();
 
-					} catch (Exception& e) {
-						creature->sendSystemMessage("Standard Format: /rotateFurniture <LEFT/RIGHT> <degrees>. Degrees can be 1 to 180 when using this format.");
-						creature->sendSystemMessage("Enhanced Format: /rotateFurniture <YAW/PITCH/ROLL> <degrees>. Degrees can be -360 to 360 when using this format.");
-						creature->sendSystemMessage("Reset Rotation to Defaults: /rotateFurniture reset 1");
-						return INVALIDPARAMETERS;
-					}
+				} catch (Exception& e) {
+					creature->sendSystemMessage("Standard Format: /rotateFurniture <LEFT/RIGHT> <degrees>. Degrees can be 1 to 180 when using this format.");
+					creature->sendSystemMessage("Enhanced Format: /rotateFurniture <YAW/PITCH/ROLL> <degrees>. Degrees can be -360 to 360 when using this format.");
+					creature->sendSystemMessage("Reset Rotation to Defaults: /rotateFurniture reset 1");
+					return INVALIDPARAMETERS;
+				}
 
-					if ((dir == "left" || dir == "right") && (degrees < 1 || degrees > 180)) {
-						creature->sendSystemMessage("Using Standard Format: The amount to rotate must be between 1 and 180.");
-						return INVALIDPARAMETERS;
-					}
+				if ((dir == "left" || dir == "right") && (degrees < 1 || degrees > 180)) {
+					creature->sendSystemMessage("Using Standard Format: The amount to rotate must be between 1 and 180.");
+					return INVALIDPARAMETERS;
+				}
 
-					if ((dir == "roll" || dir == "pitch" || dir == "yaw" ) && (degrees < -360 || degrees > 360)) {
-						creature->sendSystemMessage("Using Enhanced Format: The amount to rotate must be between -360 and 360.");
-						return INVALIDPARAMETERS;
-					}
+				if ((dir == "roll" || dir == "pitch" || dir == "yaw" ) && (degrees < -360 || degrees > 360)) {
+					creature->sendSystemMessage("Using Enhanced Format: The amount to rotate must be between -360 and 360.");
+					return INVALIDPARAMETERS;
+				}
 
-					ZoneServer* zoneServer = creature->getZoneServer();
-					ManagedReference<SceneObject*> obj = zoneServer->getObject(target);
+				ZoneServer* zoneServer = creature->getZoneServer();
+				ManagedReference<SceneObject*> obj = zoneServer->getObject(target);
 
-					if (obj == NULL || !obj->isTangibleObject() || obj->isPlayerCreature() || obj->isPet()) {
-						creature->sendSystemMessage("@player_structure:rotate_what"); //What do you want to rotate?
+				if (obj == NULL || !obj->isTangibleObject() || obj->isPlayerCreature() || obj->isPet()) {
+					creature->sendSystemMessage("@player_structure:rotate_what"); //What do you want to rotate?
+					return GENERALERROR;
+				}
+
+				ManagedReference<SceneObject*> rootParent = creature->getRootParent();
+
+				BuildingObject* buildingObject = rootParent != NULL ? (rootParent->isBuildingObject() ? cast<BuildingObject*>( rootParent.get()) : NULL) : NULL;
+				EventPerkDataComponent* data = cast<EventPerkDataComponent*>(obj->getDataObjectComponent()->get());
+
+				if (data != NULL) {
+					EventPerkDeed* deed = data->getDeed();
+
+					if (deed == NULL) {
 						return GENERALERROR;
 					}
 
+					ManagedReference<CreatureObject*> owner = deed->getOwner().get();
+
+					if (owner != creature) {
+						return GENERALERROR;
+					}
+
+				} else if (buildingObject == NULL) {
+					creature->sendSystemMessage("@player_structure:must_be_in_building"); //You must be in a building to do that.
+					return GENERALERROR;
+
+				} else {
 					if (obj->isVendor() && !obj->checkContainerPermission(creature, ContainerPermissions::MOVEVENDOR)) {
 						return GENERALERROR;
 					}
@@ -81,35 +104,40 @@ public:
 						return GENERALERROR;
 					}
 
-					if (dir == "right"){
-						obj->rotate(-degrees);
+					if (buildingObject->isGCWBase()) {
+						creature->sendSystemMessage("@player_structure:no_move_hq"); // You may not move or rotate objects inside a factional headquarters.
+						return GENERALERROR;
 					}
-					else if (dir == "left"){
-						obj->rotate(degrees);
-					}
-					else if (dir == "yaw"){
-					obj->rotate(degrees);
-					}
-					else if (dir == "pitch"){
-						obj->rotateYaxis(degrees);
-					}
-					else if (dir == "roll"){
-					obj->rotateXaxis(degrees);
-					}
-					else if (dir == "reset"){
-						obj->setDirection(1, 0, 0, 0);
-					}
-
-					obj->incrementMovementCounter();
-
-					if (obj->getParent() != NULL)
-						obj->teleport(obj->getPositionX(), obj->getPositionZ(), obj->getPositionY(), obj->getParent().get()->getObjectID());
-					else
-						obj->teleport(obj->getPositionX(), obj->getPositionZ(), obj->getPositionY());
-
-					return SUCCESS;
 				}
 
+				if (dir == "right"){
+					obj->rotate(-degrees);
+				}
+				else if (dir == "left"){
+					obj->rotate(degrees);
+				}
+				else if (dir == "yaw"){
+					obj->rotate(degrees);
+				}
+				else if (dir == "pitch"){
+					obj->rotateYaxis(degrees);
+				}
+				else if (dir == "roll"){
+					obj->rotateXaxis(degrees);
+				}
+				else if (dir == "reset"){
+					obj->setDirection(1, 0, 0, 0);
+				}
+
+				obj->incrementMovementCounter();
+
+				ManagedReference<SceneObject*> objParent = obj->getParent().get();
+				if (objParent != NULL)
+					obj->teleport(obj->getPositionX(), obj->getPositionZ(), obj->getPositionY(), objParent->getObjectID());
+				else
+					obj->teleport(obj->getPositionX(), obj->getPositionZ(), obj->getPositionY());
+
+				return SUCCESS;
 			}
 
 		};
