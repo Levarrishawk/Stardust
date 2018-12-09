2a3ac8df1f7eb171628c8253aeb83a90674c48f3
Levarris <levarrishawk@gmail.com>
2018-12-09 03:30:27 -0500

    [iteration]

--- MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
@@ -1204,12 +1204,12 @@ Vector3 SceneObjectImplementation::getCoordinate(float distance, float angleDegr
 	return Vector3(newX, newY, newZ);
 }
 
-void SceneObjectImplementation::rotateXaxis(int degrees){
+void SceneObjectImplementation::rotateXaxis(int degrees) {
 	Vector3 unity (1, 0, 0);
 	direction.rotate(unity, degrees);
 }
 
-void SceneObjectImplementation::rotateYaxis(int degrees){
+void SceneObjectImplementation::rotateYaxis(int degrees) {
 	Vector3 unity (1, 0, 0);
 	direction.rotate(unity, degrees);
 }
