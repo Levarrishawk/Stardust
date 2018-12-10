4120473922dea767c562ee3f02faab2d33793329
Levarris <levarrishawk@gmail.com>
2018-12-10 18:22:33 -0500

    [hotfix] 1.7.08 - Fishing Pole Crafting Grants Merchant XP

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/item_fishing_pole.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_fishing_pole.lua
@@ -51,8 +51,8 @@ object_draft_schematic_item_item_fishing_pole = object_draft_schematic_item_shar
    complexity = 1, 
    size = 1, 
 
-   xpType = "crafting_general", 
-   xp = 36, 
+   xpType = "merchant", 
+   xp = 360, 
 
    assemblySkill = "general_assembly", 
    experimentingSkill = "general_experimentation", 
