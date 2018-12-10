85a48530d650a1c26165d50daf7bf3a0b429774d
Levarris <levarrishawk@gmail.com>
2018-12-10 03:29:32 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/object/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/serverobjects.lua
@@ -5,5 +5,5 @@
 includeFile("custom_content/mobile/serverobjects.lua")
 includeFile("custom_content/static/serverobjects.lua")
 includeFile("custom_content/building/serverobjects.lua")
---includeFile("custom_content/tangible/serverobjects.lua")
---includeFile("custom_content/intangible/serverobjects.lua")
+includeFile("custom_content/tangible/serverobjects.lua")
+includeFile("custom_content/intangible/serverobjects.lua")
