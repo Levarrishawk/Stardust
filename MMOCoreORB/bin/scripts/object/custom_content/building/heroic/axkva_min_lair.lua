6f113deedf88c8c194c047abcafe25f1011bad2d
Levarris <levarrishawk@gmail.com>
2018-12-15 01:25:04 -0500

    [hotfix] 1.7.16 - Fixed clone bind error on wrong structure.

--- MMOCoreORB/bin/scripts/object/custom_content/building/heroic/axkva_min_lair.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/building/heroic/axkva_min_lair.lua
@@ -41,14 +41,7 @@
 --this exception also makes it possible to release a modified version 
 
 
-object_building_heroic_axkva_min_lair = object_building_heroic_shared_axkva_min_lair:new {
-	customObjectName = "Axkva Min Lair",
-	spawningPoints = {
-			{ x = 30.4, z = 0.1, y = 1.1, ow = 0.707, ox = 0, oz = 0, oy = -0.707, cellid = 1 },
-			},
-	templateType = CLONINGBUILDING,
-	planetMapCategory = "cloningfacility",
-	facilityType = CLONER_STANDARD,
+object_building_heroic_axkva_min_lair = object_building_heroic_shared_axkva_min_lair:new {	
 }
 
 ObjectTemplates:addTemplate(object_building_heroic_axkva_min_lair, "object/building/heroic/axkva_min_lair.iff")
--- MMOCoreORB/bin/scripts/screenplays/poi/isd_active_areas.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/isd_active_areas.lua
@@ -12,6 +12,7 @@ function isd_active_areas:start()
   if (isZoneEnabled("dungeon2")) then
     self:spawnSceneObjects()
     self:spawnActiveArea1()
+    self:spawnActiveArea2()
   end
 end
 
@@ -52,3 +53,65 @@ function isd_active_areas:notifySpawnArea1(pActiveArea1, pMovingObject, pPlayer)
   end)
 end
 
+function isd_active_areas:spawnActiveArea2()
+  local pSpawnArea2 = spawnSceneObject("dungeon2", "object/active_area.iff", -3086, 173, -2738, 0, 480000013)
+    
+  if (pSpawnArea2 ~= nil) then
+    local activeArea2 = LuaActiveArea(pSpawnArea2)
+          activeArea2:setCellObjectID(480000013)
+          activeArea2:setRadius(512)
+          createObserver(ENTEREDAREA, "isd_active_areas", "notifySpawnArea2", pSpawnArea2)          
+      end
+end
+
+function isd_active_areas:notifySpawnArea2(pActiveArea2, pMovingObject, pPlayer)
+  
+  if (not SceneObject(pMovingObject):isCreatureObject()) then
+    return 0
+  end
+  
+  return ObjectManager.withCreatureObject(pMovingObject, function(player)
+    if (player:isAiAgent()) then
+      return 0
+    end
+    
+    if ((player:isImperial() or player:isRebel()or player:isNeutral())) then   
+      player:playMusicMessage("sound/mus_isd_briefing.snd")
+      
+      end
+    return 0    
+  end)
+end
+
+function republic_corvette:spawnActiveArea3()
+  local pSpawnArea3 = spawnSceneObject("dungeon2", "object/active_area.iff", -3000, 453, -2672, 0, 480000055)
+    
+  if (pSpawnArea3 ~= nil) then
+    local activeArea3 = LuaActiveArea(pSpawnArea3)
+          activeArea3:setCellObjectID(480000055)
+          activeArea3:setRadius(512)
+          createObserver(ENTEREDAREA, "republic_corvette", "notifySpawnArea3", pSpawnArea3)
+          --createObserver(EXITEDAREA, "ChandrilTechScreenPlay", "notifySpawnAreaLeave", pSpawnArea1)
+      end
+end
+
+function republic_corvette:notifySpawnArea3(pActiveArea3, pMovingObject, pPlayer)
+  
+  if (not SceneObject(pMovingObject):isCreatureObject()) then
+    return 0
+  end
+  
+  return ObjectManager.withCreatureObject(pMovingObject, function(player)
+    if (player:isAiAgent()) then
+      return 0
+    end
+    
+    if ((player:isImperial() or player:isRebel()or player:isNeutral())) then
+
+      --player:sendSystemMessage("ATTENTION: Worker Revolt in Progress on Level Three.  Security to Level Three.")
+      player:playMusicMessage("sound/mus_isd_hangar2.snd")
+      
+      end
+    return 0    
+  end)
+end
