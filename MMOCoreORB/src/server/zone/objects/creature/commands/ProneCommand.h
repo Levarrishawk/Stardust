73bca5adf20002b4a792e38b7fae4b49423764a1
Levarrishawk <levarrishawk@gmail.com>
2018-12-20 21:33:46 -0500

    [iteration]

--- MMOCoreORB/src/server/zone/objects/creature/commands/ProneCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ProneCommand.h
@@ -49,9 +49,8 @@ public:
 				ZoneServer* zserv = server->getZoneServer();
 
 				String blueFrogTemplate = "object/tangible/terminal/terminal_character_builder.iff";
-				String blueFrogTemplate2 = "object/tangible/terminal/terminal_character_builder2.iff";
 
-				ManagedReference<CharacterBuilderTerminal*> blueFrog = (( zserv->createObject(blueFrogTemplate.hashCode(), 0)).castTo<CharacterBuilderTerminal*>() || zserv->createObject(blueFrogTemplate2.hashCode(), 0)).castTo<CharacterBuilderTerminal*>() ) ;
+				ManagedReference<CharacterBuilderTerminal*> blueFrog = ( zserv->createObject(blueFrogTemplate.hashCode(), 0)).castTo<CharacterBuilderTerminal*>();
 
 				if (blueFrog == NULL)
 					return GENERALERROR;
