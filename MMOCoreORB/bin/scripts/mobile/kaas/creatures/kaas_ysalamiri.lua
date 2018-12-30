1a5dfaf786575b40f9cc5237a6ffe5a37162df17
Levarrishawk <levarrishawk@gmail.com>
2018-12-29 21:09:54 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/kaas/creatures/kaas_ysalamiri.lua
+++ MMOCoreORB/bin/scripts/mobile/kaas/creatures/kaas_ysalamiri.lua
@@ -1,6 +1,6 @@
 ysalamiri = Creature:new {
-	objectName = "",
-	customName = "a Ysalamiri",	
+	objectName = "@mob/creature_names:ysalamiri",
+	--customName = "a Ysalamiri",	
 	socialGroup = "snake",
 	pvpFaction = "",
 	faction = "",
