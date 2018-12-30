0553d625fa7e82e9e95e3c9e5c1b8bc08063f205
Levarrishawk <levarrishawk@gmail.com>
2018-12-30 18:59:41 -0500

    [hotfix] 1.8.18 - Adjusted Krayt Damage / Loot Tables

--- MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_adolescent.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_adolescent.lua
@@ -2,10 +2,10 @@ krayt_dragon_adolescent = Creature:new {
 	objectName = "@mob/creature_names:krayt_dragon_adolescent",
 	socialGroup = "krayt",
 	faction = "",
-	level = 160,
+	level = 200,
 	chanceHit = 10,
 	damageMin = 445,
-	damageMax = 1200,
+	damageMax = 1600,
 	baseXp = 28549,
 	baseHAM = 72000,
 	baseHAMmax = 90000,
@@ -28,32 +28,17 @@ krayt_dragon_adolescent = Creature:new {
 	templates = {"object/mobile/krayt_dragon_hue.iff"},
 	scale = 0.7,
 	lootGroups = {
-		{
-	        groups = {
-				{group = "krayt_tissue_rare", chance = 2000000},
-				{group = "krayt_dragon_common", chance = 3000000},
-				{group = "armor_all", chance = 2500000},
-				{group = "weapons_all", chance = 1750000},
-				{group = "armor_attachments", chance = 750000},   -- 100% * 7.5% = 7.5%
-			},
-			lootChance = 10000000,  -- 100%
-		},
-		{
-	        groups = {
-				{group = "krayt_dragon_common", chance = 3000000},
-				{group = "armor_all", chance = 3500000},
-				{group = "weapons_all", chance = 3500000},
-			},
-			lootChance = 3500000,
-		},
-		-- Pearl groups
-		{
-	        groups = {
-				{group = "krayt_pearls", chance = 10000000},
-			},
-			lootChance = 1500000 -- 15% chance for this group
-		},
-	},
+    {
+          groups = {
+        {group = "krayt_tissue_rare", chance = 2000000},
+        {group = "krayt_dragon_common", chance = 2500000},
+        {group = "krayt_pearls", chance = 1500000},
+        {group = "armor_all", chance = 2000000},
+        {group = "weapons_all", chance = 2000000},
+      },
+      lootChance = 7720000
+    }
+},
 	weapons = {},
 	conversationTemplate = "",
 	attacks = {
