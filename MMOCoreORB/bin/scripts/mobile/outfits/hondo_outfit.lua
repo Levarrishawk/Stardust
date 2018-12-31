e41d81c0dd783ef118fb17503cd96da73069fed1
Levarrishawk <levarrishawk@gmail.com>
2018-12-31 03:02:50 -0500

    [iteration]

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/hondo_outfit.lua
@@ -0,0 +1,12 @@
+hondo_outfit = {
+	{ 
+		{objectTemplate = "object/tangible/wearables/shirt/shirt_s15.iff", customizationVariables = {{"/private/index_color_1", 215}} },
+		{objectTemplate = "object/tangible/wearables/pants/pants_s01.iff", customizationVariables = {{"/private/index_color_2", 243}, {"/private/index_color_1", 243}} },
+		{objectTemplate = "object/tangible/wearables/boots/boots_s04.iff", customizationVariables = {{"/private/index_color_1", 87}} },
+		{objectTemplate = "object/tangible/wearables/jacket/jacket_lifeday_figrin_dan.iff", customizationVariables = {{"/private/index_color_1", 243}} },
+		
+	}
+	 
+}
+
+addOutfitGroup("hondo_outfit", hondo_outfit)
