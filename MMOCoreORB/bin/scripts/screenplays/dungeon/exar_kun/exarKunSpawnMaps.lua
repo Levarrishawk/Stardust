3d50e34398cc63b30fb003eca8337ef0a70917d5
Levarrishawk <levarrishawk@gmail.com>
2018-12-31 21:57:05 -0500

    [iteration] Exar Kun Instance
    Foundational work to build out an instanced dungeon.

---
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/exar_kun/exarKunSpawnMaps.lua
@@ -0,0 +1,11 @@
+exarKunStaticSpawns = {
+	-- { template, x, z, y, roomName, yaw, customName, functionName, functionData, factionOnlySpawn }
+	
+}
+
+exarKunSpawns = {
+-- { template, x, z, y, yaw, cellName, functionName}
+	{ "insane_vitiate_cultist", -11.7, -0.4, -55.4, 179.18, "r2", "" },   -- placeholder
+}
+
+
