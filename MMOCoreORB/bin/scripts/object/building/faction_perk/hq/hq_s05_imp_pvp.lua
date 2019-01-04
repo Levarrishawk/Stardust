869d5f807a2d47cdcc593f97e21e022d8e07fccd
Levarrishawk <levarrishawk@gmail.com>
2019-01-04 04:37:28 -0500

    [hotfix] 1.8.25 - Allow GCW Bases on New Worlds

--- MMOCoreORB/bin/scripts/object/building/faction_perk/hq/hq_s05_imp_pvp.lua
+++ MMOCoreORB/bin/scripts/object/building/faction_perk/hq/hq_s05_imp_pvp.lua
@@ -42,7 +42,9 @@
 
 
 object_building_faction_perk_hq_hq_s05_imp_pvp = object_building_faction_perk_hq_shared_hq_s05_imp_pvp:new {
-
+    lotSize = 1,
+    maintenanceCost = 1337,
+    allowedZones = {0,1,4,5,6,7,8,21,22,23,25}
 }
 
 ObjectTemplates:addTemplate(object_building_faction_perk_hq_hq_s05_imp_pvp, "object/building/faction_perk/hq/hq_s05_imp_pvp.iff")
