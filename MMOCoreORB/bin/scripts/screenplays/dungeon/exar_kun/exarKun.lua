b37809f82b9be51c79708347e1089be61d1db96a
Levarris <levarrishawk@gmail.com>
2019-01-07 00:04:40 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/dungeon/exar_kun/exarKun.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/exar_kun/exarKun.lua
@@ -31,7 +31,9 @@ function exarKun:activate(pPlayer, faction, questType)
   writeData("exarKunStartTime:", os.time())
   writeData("exarKun:occupiedState", 1)  -- TO DO: Need to create the timer and conditions to reset the state of the instance.
   
-  createEvent(5 * 60 * 1000, "exarKun", "handleTimer", "")
+  local pExarKun = getSceneObject(480000292)
+  
+  createEvent(5 * 60 * 1000, "exarKun", "handleTimer", pExarKun, "")
   
 	if (CreatureObject(pPlayer):isGrouped()) then
 		local groupSize = CreatureObject(pPlayer):getGroupSize()
@@ -103,40 +105,40 @@ function exarKun:transportPlayer(pPlayer)
 	SceneObject(pPlayer):switchZone("yavin4", -11.8, 0.2, -121.8, 480000293)
 end
 
-function exarKun:handleTimer(pCell)
+function exarKun:handleTimer(pExarKun)
   local startTime = readData("exarKunStartTime:")
   local timeLeftSecs = 3600 - (os.time() - startTime)
   local timeLeft = math.floor(timeLeftSecs / 60)
 
   if (timeLeft > 10) then
     self:broadcastToPlayers( "@dungeon/corvette:timer_" .. timeLeft)
-    createEvent(5 * 60 * 1000, "exarKun", "handleTimer", "")
+    createEvent(5 * 60 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeft >= 3) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_" .. timeLeft)
-    createEvent(60 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_" .. timeLeft)
+    createEvent(60 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeft >= 2) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_" .. timeLeft)
-    createEvent(30 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_" .. timeLeft)
+    createEvent(30 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeftSecs >= 90) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_90s")
-    createEvent(30 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_90s")
+    createEvent(30 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeftSecs >= 60) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_1")
-    createEvent(30 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_1")
+    createEvent(30 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeftSecs >= 30) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_30s")
-    createEvent(20 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_30s")
+    createEvent(20 * 1000, "exarKun", "handleTimer", pExarKun, "")
   elseif (timeLeftSecs >= 10) then
-    self:broadcastToPlayers(pCell, "@dungeon/corvette:timer_10s")
-    createEvent(10 * 1000, "exarKun", "handleTimer", "")
+    self:broadcastToPlayers(pExarKun, "@dungeon/corvette:timer_10s")
+    createEvent(10 * 1000, "exarKun", "handleTimer", pExarKun, "")
   else
     self:resetInstance()
   end
 end
 
-function exarKun:broadcastToPlayers(message)
+function exarKun:broadcastToPlayers(pExarKun, message)
   for i = 1, 66, 1 do
-    local pCell = BuildingObject(480000292)
+    local pCell = BuildingObject(pExarKun):getCell(i)
 
     if (pCell ~= nil) then
       for j = 1, SceneObject(pCell):getContainerObjectsSize(), 1 do
