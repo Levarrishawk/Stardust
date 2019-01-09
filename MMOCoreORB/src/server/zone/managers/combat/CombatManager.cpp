ce294c5d34b8579cc1da811bdf62d212b4f2e613
Levarrishawk <levarrishawk@gmail.com>
2019-01-08 21:29:29 -0500

    [iteration]

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -861,7 +861,7 @@ float CombatManager::getDefenderToughnessModifier(CreatureObject* defender, int 
 	}
 
 	int jediToughness = defender->getSkillMod("jedi_toughness");
-	//if (damType != SharedWeaponObjectTemplate::LIGHTSABER && jediToughness > 0)   //jedi toughness will affect all damage types
+	if (jediToughness > 0)   //jedi toughness will affect all damage types
 		damage *= 1.f - (jediToughness / 200.f);
 
 	return damage < 0 ? 0 : damage;
