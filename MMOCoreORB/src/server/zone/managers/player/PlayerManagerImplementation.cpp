8a3a7c3da4966e048e85efcc8960c82b9af7527f
Levarris <levarrishawk@gmail.com>
2019-01-15 19:20:31 -0500

    [iteration]

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -1363,11 +1363,11 @@ void PlayerManagerImplementation::disseminateExperience(TangibleObject* destruct
 				if (winningFaction == attacker->getFaction())
 					xpAmount *= gcwBonus;
 
-				//Jedi experience doesn't count towards combat experience, and is earned at 20% the rate of normal experience
+				//Jedi experience doesn't count towards combat experience, and is earned at 50% the rate of normal experience
 				if (xpType != "jedi_general")
 					combatXp += xpAmount;
 				else
-					xpAmount *= 0.2f;
+					xpAmount *= 0.5f;
 
 				//Award individual expType
 				awardExperience(attacker, xpType, xpAmount);
