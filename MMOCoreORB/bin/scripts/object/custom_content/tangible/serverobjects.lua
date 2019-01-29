6f181c15296d1b3109781ba4a42ffd7f3961fd3f
Levarrishawk <levarrishawk@gmail.com>
2019-01-28 20:33:31 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/serverobjects.lua
@@ -51,11 +51,11 @@ includeFile("custom_content/tangible/wearables/serverobjects.lua")
 --
 
 includeFile("custom_content/tangible/collection/serverobjects.lua")
---[[
 includeFile("custom_content/tangible/dungeon/serverobjects.lua")
 includeFile("custom_content/tangible/hologram/serverobjects.lua")
 includeFile("custom_content/tangible/item/serverobjects.lua")
+includeFile("custom_content/tangible/loot/serverobjects.lua")
 includeFile("custom_content/tangible/quest/serverobjects.lua")
 includeFile("custom_content/tangible/npe/serverobjects.lua")
---]]
+
 
