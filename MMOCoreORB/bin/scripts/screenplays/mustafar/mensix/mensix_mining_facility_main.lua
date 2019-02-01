9ff78a02959b7f8a8c3337c44adc39bd49dc3db0
Levarrishawk <levarrishawk@gmail.com>
2019-01-31 23:16:34 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/mustafar/mensix/mensix_mining_facility_main.lua
+++ MMOCoreORB/bin/scripts/screenplays/mustafar/mensix/mensix_mining_facility_main.lua
@@ -39,7 +39,7 @@ function mensix_mining_facility_main:touristConvoStart(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_f)
   
-  spatialChat(pTraveler_f, "@must_mining_traveler:cant_believe")
+  spatialChat(pTraveler_f, "I cannot believe you took me to this flaming hunk of rock! What were you thinking? This world is a nightmare.") -- I cannot believe you took me to this flaming hunk of rock! What were you thinking? This world is a nightmare.
   createEvent(10000, "mensix_mining_facility_main", "touristConvo1", pTraveler_m, "")
 end
 
@@ -47,7 +47,7 @@ function mensix_mining_facility_main:touristConvo1(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_f)
   
-  spatialChat(pTraveler_m, "@must_mining_traveler:do_not_start")
+  spatialChat(pTraveler_m, "Please, Clarrisa, don't start. I thought this would be a nice change of pace for us. You said you wanted to go some place full of adventure and mystery. With all of the discoveries here on Mustafar I thought you would love it.") -- Please, Clarrisa, don't start. I thought this would be a nice change of pace for us. You said you wanted to go some place full of adventure and mystery. With all of the discoveries here on Mustafar I thought you would love it.
   createEvent(10000, "mensix_mining_facility_main", "touristConvo2", pTraveler_f, "")
 end
 
@@ -55,7 +55,7 @@ function mensix_mining_facility_main:touristConvo2(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_f)
   
-  spatialChat(pTraveler_f, "@must_mining_traveler:want_to_leave")
+  spatialChat(pTraveler_f, "Adventure! When I said that, I meant we should go someplace nice like Naboo. You drag me half way across the galaxy to show me a burning rock! We are going to have a serious talk about your concept of adventure when we get home. Are you listening to me?") -- Adventure! When I said that, I meant we should go someplace nice like Naboo. You drag me half way across the galaxy to show me a burning rock! We are going to have a serious talk about your concept of adventure when we get home. Are you listening to me?
   createEvent(10000, "mensix_mining_facility_main", "touristConvo3", pTraveler_m, "")
 end
 
@@ -63,7 +63,7 @@ function mensix_mining_facility_main:touristConvo3(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_f)
   
-  spatialChat(pTraveler_m, "@must_mining_traveler:of_course_dear")
+  spatialChat(pTraveler_m, "Of course dear. I was just trying to be exciting and unexpected for you. We could have explored some of those ruins and maybe make a discovery of our own.") -- Of course dear. I was just trying to be exciting and unexpected for you. We could have explored some of those ruins and maybe make a discovery of our own.
   createEvent(10000, "mensix_mining_facility_main", "touristConvo4", pTraveler_f, "")
 end
 
@@ -71,7 +71,7 @@ function mensix_mining_facility_main:touristConvo4(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_f)
   
-  spatialChat(pTraveler_m, "@must_mining_traveler:we_better")
+  spatialChat(pTraveler_m, "Next time you want to be exciting...don't. I will not have any such foolishness like you digging around in the dirt like some grubby archeologist. What would people back home say? Now we are just going to wait until the next shuttle and never speak of this again. Is that understood?") -- Next time you want to be exciting...don't. I will not have any such foolishness like you digging around in the dirt like some grubby archeologist. What would people back home say? Now we are just going to wait until the next shuttle and never speak of this again. Is that understood?
   createEvent(12000, "mensix_mining_facility_main", "touristConvo5", pTraveler_m, "")
 end
 
@@ -79,6 +79,6 @@ function mensix_mining_facility_main:touristConvo5(pTraveler_m, pTraveler_f)
   local husband = LuaCreatureObject(pTraveler_m)
   local wife = LuaCreatureObject(pTraveler_m)
   
-  spatialChat(pTraveler_m, "@must_mining_traveler:yes_dear")
+  spatialChat(pTraveler_m, "Yes, dear. Whatever you say.") -- Yes, dear. Whatever you say.
   createEvent(600000, "mensix_mining_facility_main", "touristConvoStart", pTraveler_f, "")
 end
