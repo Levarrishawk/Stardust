7297d5397623ccd97ace834643696f6ebc935310
Levarrishawk <levarrishawk@gmail.com>
2019-02-02 04:24:38 -0500

    [hotfix] 1.9.19

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -820,6 +820,7 @@ includeFile("events/axkvaMinEntryMenuComponent.lua")
 --]]
 
 -- Mustafar
+--[[
 includeFile("mustafar/boundaries/mustafar_boundaries.lua")
 includeFile("mustafar/mensix/mensix_mining_facility_main.lua")
-
+--]]
