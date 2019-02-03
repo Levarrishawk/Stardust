2e05571ef2da03e0df9a5bf1e84789d3eb7e9bf8
Levarris <levarrishawk@gmail.com>
2019-02-02 23:08:36 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/mustafar/mensix/mensix_mining_facility_main.lua
+++ MMOCoreORB/bin/scripts/screenplays/mustafar/mensix/mensix_mining_facility_main.lua
@@ -13,9 +13,12 @@ function mensix_mining_facility_main:start()
 		writeData("mensix_mining_facility_main:travelerConvoInProgress", 0) 
 		writeData("mensix_mining_facility_main:travelerConvoState", 0)   
 		
+		writeData("mensix_mining_facility_main:minerConvoInProgress", 0) 
+    writeData("mensix_mining_facility_main:minerConvoState", 0)  
+    
 		self:spawnMobiles()
 		self:spawnSceneObjects()
-		--self:spawnTravelerConvoActiveArea()
+		self:spawnMinerConvoActiveArea()
 		self:startTravelerConvo()
 	end
 end
@@ -32,45 +35,21 @@ end
 
 function mensix_mining_facility_main:spawnMobiles()
 
-    local pTraveler_m = spawnMobile("mustafar", "traveler_m",0,-2481,230.1,1633.7,-67,0)  -- -55.1,31.5,-120.3,-33,12112248  Original NGE Position (changed to outdoors due to spatialChat not working in cell)
+    local pTraveler_m = spawnMobile("mustafar", "traveler_m",0,-2481,230.1,1633.7,-51,0)  -- -55.1,31.5,-120.3,-33,12112248  Original NGE Position (changed to outdoors due to spatialChat not working in cell)
       self:setMoodString(pTraveler_m, "npc_consoling")    
     local pTraveler_f = spawnMobile("mustafar", "traveler_f",0,-2483.1,230.1,1635.7,-90,0)  -- -56.7,31.5,-118.9,-90,12112248 Original NGE Position (changed to outdoors due to spatialChat not working in cell)
       self:setMoodString(pTraveler_f, "angry")    
       
      writeData("mensix_mining_facility_main:traveler_m_objectID", SceneObject(pTraveler_m):getObjectID() )
      writeData("mensix_mining_facility_main:traveler_f_objectID", SceneObject(pTraveler_f):getObjectID() )    
-  
-end
-
-function mensix_mining_facility_main:spawnTravelerConvoActiveArea()  -- Active areas use world coords.   Set to actual world coord in each instance manually.
-  local pActiveArea1 = spawnSceneObject("mustafar", "object/active_area.iff", -2475, 230, 1648, 0, 12112248)
-  if (pActiveArea1 ~= nil) then
-    local activeArea = LuaActiveArea(pActiveArea1)
-          activeArea:setCellObjectID(12112248)
-          activeArea:setRadius(15)
-          createObserver(ENTEREDAREA, "mensix_mining_facility_main", "notifyTravelerConvoActiveArea", pActiveArea1)
-                  
-      end
-end
-
-function mensix_mining_facility_main:notifyTravelerConvoActiveArea(pActiveArea1, pMovingObject, pPlayer, pTraveler_m, pTraveler_f)
-  
-  if (not SceneObject(pMovingObject):isCreatureObject()) then
-    return 0
-  end
-  
-  return ObjectManager.withCreatureObject(pMovingObject, function(player)
-    if (player:isAiAgent()) then
-      return 0
-    end
-    
-    if ((player:isImperial() or player:isRebel()or player:isNeutral())) then
-
-      self:startTravelerConvo()
-      
-      end
-    return 0    
-  end)
+     
+     local pMiner1 = spawnMobile("mustafar", "mustafar_miner_02",0,-2384.0,210.2,1809.9,-35,0)
+     local pMiner2 = spawnMobile("mustafar", "mustafar_miner_01",0,-2384.3,210.2,1813.2,165,0)
+     local pMiner3 = spawnMobile("mustafar", "mustafar_miner_01",0,-2387.6,210.2,1812.2,123,0)
+     
+     writeData("mensix_mining_facility_main:pMiner1_objectID", SceneObject(pMiner1):getObjectID() )
+     writeData("mensix_mining_facility_main:pMiner2_objectID", SceneObject(pMiner2):getObjectID() )
+     writeData("mensix_mining_facility_main:pMiner3_objectID", SceneObject(pMiner3):getObjectID() )
 end
 
 
@@ -154,14 +133,59 @@ function mensix_mining_facility_main:touristConvoM3(pTraveler_m, pPlayer)
   if (readData("mensix_mining_facility_main:travelerConvoState") == 5) then
       spatialChat(pTraveler_m, "Yes, dear. Whatever you say.") 
         writeData("mensix_mining_facility_main:travelerConvoState", 0)   
-        createEvent(6 * 100 * 1000, "mensix_mining_facility_main", "resetConvo", pTraveler_m, "")   
+        createEvent(6 * 100 * 1000, "mensix_mining_facility_main", "resetTravelerConvo", pTraveler_m, "")   
   end
 end
 
-
-
-function mensix_mining_facility_main:resetConvo(pPlayer, pTraveler_f, pTraveler_m)
+function mensix_mining_facility_main:resetTravelerConvo(pPlayer, pTraveler_f, pTraveler_m)
     writeData("mensix_mining_facility_main:travelerConvoInProgress", 0)    
     self:startTravelerConvo()
+end
+
+
+function mensix_mining_facility_main:spawnMinerConvoActiveArea()  -- Active areas use world coords.   Set to actual world coord in each instance manually.
+  local pActiveArea1 = spawnSceneObject("mustafar", "object/active_area.iff", -2384, 210, 1811, 0, 0)
+  if (pActiveArea1 ~= nil) then
+    local activeArea = LuaActiveArea(pActiveArea1)
+          activeArea:setCellObjectID(0)
+          activeArea:setRadius(20)
+          createObserver(ENTEREDAREA, "mensix_mining_facility_main", "notifyMinerConvoActiveArea", pActiveArea1)
+                  
+      end
+end
+
+function mensix_mining_facility_main:notifyMinerConvoActiveArea(pActiveArea1, pMovingObject, pPlayer, pTraveler_m, pTraveler_f)
+  
+  if (not SceneObject(pMovingObject):isCreatureObject()) then
+    return 0
+  end
+  
+  return ObjectManager.withCreatureObject(pMovingObject, function(player)
+    if (player:isAiAgent()) then
+      return 0
+    end
+    
+    if ((player:isImperial() or player:isRebel()or player:isNeutral())) then
+
+      self:startMinerConvo()
+      
+      end
+    return 0    
+  end)
+end
+
+function mensix_mining_facility_main:startMinerConvo(pActiveArea1, pMovingObject, pPlayer, pMiner1, pMiner2, pMiner3)
+  
+   local pMiner1 = getSceneObject(readData("mensix_mining_facility_main:pMiner1_objectID"))
+   local pMiner2 = getSceneObject(readData("mensix_mining_facility_main:pMiner2_objectID"))
+   local pMiner3 = getSceneObject(readData("mensix_mining_facility_main:pMiner3_objectID"))
+
+   if not(readData("mensix_mining_facility_main:minerConvoInProgress") == 1) then       
+          writeData("mensix_mining_facility_main:minerConvoInProgress", 1)
+       --   createEvent(90 * 1000, "mensix_mining_facility_main", "touristConvoF1", pTraveler_f, "")
+       --   createEvent(100 * 1000, "mensix_mining_facility_main", "touristConvoM1", pTraveler_m, "")
+   else
+      return 0
+   end              
 end
 
