56ad6e059425e4e9fb8b09a6552842f625f7beb8
Levarrishawk <levarrishawk@gmail.com>
2019-02-10 00:00:26 -0500

    [iteration]

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceIntimidate1Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceIntimidate1Command.h
@@ -43,6 +43,14 @@ public:
 
 		CreatureObject *tarCreo = targetObject->asCreatureObject();
 
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		CreatureObject* targetCreature = dynamic_cast<CreatureObject*>(object.get());
+
+		Locker clocker(targetCreature, creature);
+
+		ManagedReference<PlayerObject*> player = creature->getPlayerObject();
+		PlayerObject* targetPlayerObject = targetCreature->getPlayerObject();
+
 		int res = doCombatAction(creature, target);
 
 		if(res == SUCCESS) {
