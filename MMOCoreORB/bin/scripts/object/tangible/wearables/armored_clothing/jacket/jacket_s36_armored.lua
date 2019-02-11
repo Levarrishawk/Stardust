e09518acc605bbcbf0af408d992b256be11ac6eb
Levarrishawk <levarrishawk@gmail.com>
2019-02-11 01:08:21 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armored_clothing/jacket/jacket_s36_armored.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armored_clothing/jacket/jacket_s36_armored.lua
@@ -80,4 +80,4 @@ object_tangible_wearables_armored_clothing_jacket_jacket_s36_armored = object_ta
   experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
-ObjectTemplates:addTemplate(object_tangible_wearables_armored_clothing_jacket_jacket_s26_armored, "object/tangible/wearables/armored_clothing/jacket/jacket_s36_armored.iff")
+ObjectTemplates:addTemplate(object_tangible_wearables_armored_clothing_jacket_jacket_s36_armored, "object/tangible/wearables/armored_clothing/jacket/jacket_s36_armored.iff")
