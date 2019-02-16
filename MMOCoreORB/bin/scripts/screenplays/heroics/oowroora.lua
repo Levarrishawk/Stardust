b8a135d4b7c3a783f94202d66c006df0d788f66a
Levarrishawk <levarrishawk@gmail.com>
2019-02-16 05:24:30 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/heroics/oowroora.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/oowroora.lua
@@ -15,7 +15,7 @@ registerScreenPlay("oowroora", true) --Initialize the screenplay IAW the screenp
 
 --Simply spawns the NPC
 function oowroora:start()
-   local pNpc = spawnMobile("lothal", "oowroora", 1, 1313, 28, -3077, 90, 0)
+   local pNpc = spawnMobile("lothal", "oowroora", 1, -1213, 28, -3077, 90, 0)
    self:setMoodString(pNpc, "neutral")
 end
 
