d1defb741b6fde3a20eedf7eb2c9f88874691741
Levarrishawk <levarrishawk@gmail.com>
2019-02-19 23:51:23 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/events/elysiumTerminalMenuComponent_01.lua
+++ MMOCoreORB/bin/scripts/screenplays/events/elysiumTerminalMenuComponent_01.lua
@@ -18,15 +18,16 @@ function elysiumTerminalMenuComponent_01:handleObjectMenuSelect(pSceneObject, pP
 	end
 
 	if selectedID == 20 then
-	  
-	  local sui = SuiMessageBox.new("elysiumTerminalMenuComponent_01", "handleTeleport")
-    sui.setPrompt("As you touch the Crystal it begins to hum and you feel a strange pulling sensation in the force. It is not so strong that you could not back away from it now, but the pull grows stronger as you hold your hand on it's surface.  (This will transport you to another location.  Do you wish to proceed?)")
-    sui.setTitle("Strange Crystal")
-    sui.setCancelButtonText("Cancel") -- Cancel  
-    sui.setOkButtonText("OK") -- OK   
-
-    sui.sendTo(pPlayer)   	  		 	
-    
+	  if (CreatureObject(pPlayer):hasSkill("force_title_jedi_rank_03")) then
+      local sui = SuiMessageBox.new("elysiumTerminalMenuComponent_01", "handleTeleport")
+      sui.setPrompt("As you touch the Crystal it begins to hum and you feel a strange pulling sensation in the force. It is not so strong that you could not back away from it now, but the pull grows stronger as you hold your hand on it's surface.  (This will transport you to another location.  Do you wish to proceed?)")
+      sui.setTitle("Strange Crystal")
+      sui.setCancelButtonText("Cancel") -- Cancel  
+      sui.setOkButtonText("OK") -- OK   
+      sui.sendTo(pPlayer) 
+    else
+      CreatureObject(pPlayer):sendSystemMessage("The crystal hums briefly as you place your hand on it.  You sense there is more to this if you only had the right knowledge.")   
+    end	    	  		     
 	end
 
 	return 0
