2d65ea76761972e41b93f839b0a2b0fd65be6fc8
Levarris <levarrishawk@gmail.com>
2019-02-27 06:26:44 -0500

    [iteration] Added Cafe Table Loot Schematic

--- MMOCoreORB/bin/scripts/loot/items.lua
+++ MMOCoreORB/bin/scripts/loot/items.lua
@@ -459,7 +459,7 @@ includeFile("items/loot_schematic/tatooine_tapestry_schematic.lua")
 includeFile("items/loot_schematic/technical_console_schematic_1.lua")
 includeFile("items/loot_schematic/technical_console_schematic_2.lua")
 includeFile("items/loot_schematic/throw_pillow_schematic.lua")
---includeFile("items/loot_schematic/cortosis_grenade_loot_schematic.lua")
+includeFile("items/loot_schematic/table_coffee_modern_02_schematic.lua")
 
 
 
