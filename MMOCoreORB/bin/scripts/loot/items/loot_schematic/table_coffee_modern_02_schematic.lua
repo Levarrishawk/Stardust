11a797278f109b287b723eaaa28eb4a32578437b
Levarris <levarrishawk@gmail.com>
2019-02-27 22:42:31 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/loot/items/loot_schematic/table_coffee_modern_02_schematic.lua
+++ MMOCoreORB/bin/scripts/loot/items/loot_schematic/table_coffee_modern_02_schematic.lua
@@ -2,7 +2,7 @@ table_coffee_modern_02_schematic = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	customObjectName = "",
-	directObjectTemplate = "object/tangible/loot/loot_schematic/table_coffee_modern_02_schematic.iff",
+	directObjectTemplate = "object/tangible/loot/loot_schematic/table_coffee_modern_schematic.iff",
 	craftingValues = {
 	},
 	customizationStringNames = {},
