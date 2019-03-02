bccbf1784908713edcb67de37d7c8d5c46a9161c
Levarrishawk <levarrishawk@gmail.com>
2019-03-02 02:20:59 -0500

    [itertation]

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ns_hat_s01.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ns_hat_s01.lua
@@ -65,7 +65,7 @@ object_draft_schematic_clothing_clothing_ns_hat_s01 = object_draft_schematic_clo
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"trim_and_binding", "trim", "rancor_hide", "hardware", "skirt", "bodice"},
    ingredientSlotType = {0, 1, 1, 0, 1, 1},
-   resourceTypes = {"bone_mammal", "object/tangible/component/clothing/shared_trim.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "metal", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceTypes = {"bone", "object/tangible/component/clothing/shared_trim.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "metal", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
    resourceQuantities = {14, 2, 3, 8, 3, 1},
    contribution = {100, 100, 100, 100, 100, 100},
 
