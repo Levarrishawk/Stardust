2418de44057ada061d4999099ca5d9e457395a87
Levarrishawk <levarrishawk@gmail.com>
2019-03-05 21:17:31 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/loot/items/force_crystal_ahsokas_purity.lua
+++ MMOCoreORB/bin/scripts/loot/items/force_crystal_ahsokas_purity.lua
@@ -13,7 +13,7 @@ force_crystal_ahsokas_purity= {
 		{"attackactioncost",0,5,0},
 		{"attackmindcost",0,5,0},
 		{"forcecost",0,4,0},
-		{"color",31,31,0},
+		{"color",63,63,0},
 		{"quality",7,7,0},
 	},
 	customizationStringNames = {},
