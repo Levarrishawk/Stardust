b8f3db9ddbd40884fed878a432d8c0cda6363189
Levarris <levarrishawk@gmail.com>
2019-03-08 06:13:01 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/outfits/sabine_wren_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/sabine_wren_outfit.lua
@@ -1,12 +1,12 @@
 sabine_wren_outfit = {
 	{ 
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bicep_l.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bicep_r.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_boots.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bracer_l.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bracer_r.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_chest_plate.iff"},
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_leggings.iff"}
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bicep_l.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bicep_r.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_boots.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bracer_l.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_bracer_r.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_chest_plate.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_sabine/armor_mandalorian_sabine_leggings.iff", customizationVariables = {} },
 	}
 	 
 }
