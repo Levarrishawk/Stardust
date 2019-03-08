f6a17ce387a6ed18e409fbc11cae8f487d1c1aee
Levarris <levarrishawk@gmail.com>
2019-03-08 00:31:22 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/townsperson/sabine_wren.lua
+++ MMOCoreORB/bin/scripts/mobile/townsperson/sabine_wren.lua
@@ -32,8 +32,8 @@ sabine_wren = Creature:new {
 					--"object/mobile/dressed_commoner_naboo_human_female_04.iff",
 					--"object/mobile/dressed_commoner_naboo_human_female_05.iff",
 					--"object/mobile/dressed_commoner_naboo_human_female_06.iff", --
-					"object/mobile/dressed_commoner_naboo_human_female_07.iff",
-					--"object/mobile/dressed_commoner_naboo_human_female_08.iff",				
+					--"object/mobile/dressed_commoner_naboo_human_female_07.iff",
+					"object/mobile/dressed_commoner_naboo_human_female_08.iff",				
 					},
 				
 	lootGroups = {},
