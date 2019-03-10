8d555d4ad0c6b0218f7e3a493ebb33c7d33daba5
Levarrishawk <levarrishawk@gmail.com>
2019-03-10 19:40:18 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/object/building/player/yt1300_house.lua
+++ MMOCoreORB/bin/scripts/object/building/player/yt1300_house.lua
@@ -50,6 +50,8 @@ object_building_player_yt1300_house = object_building_player_shared_yt1300_house
 		{"private_medical_rating", 100},
     {"private_med_wound_health", 100},
     {"private_med_wound_action", 100},
+    {"private_buff_mind", 100},
+    {"private_med_battle_fatigue", 5},
     {"private_safe_logout", 1}
 	},
 	childObjects = {
