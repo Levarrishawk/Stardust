bd1897eea4b1a8b7d325dccf505beda450f18c3f
scscofield <50122748+scscofield@users.noreply.github.com>
2019-08-08 20:04:15 -0400

    corlorpush

--- MMOCoreORB/src/server/zone/managers/components/ComponentManager.cpp
+++ MMOCoreORB/src/server/zone/managers/components/ComponentManager.cpp
@@ -113,6 +113,7 @@
 #include "server/zone/objects/tangible/components/ThrowGrenadeMenuComponent.h"
 #include "server/zone/objects/tangible/components/GogglesObjectMenuComponent.h"
 #include "server/zone/objects/tangible/components/ArmorObjectMenuComponent.h"
+#include "server/zone/objects/tangible/components/ArmorObjectMenuComponent1.h"
 #include "server/zone/objects/tangible/components/PlaceableLootContainerComponent.h"
 #include "server/zone/objects/tangible/components/ScavengerChestContainerComponent.h"
 #include "server/zone/objects/tangible/components/ScavengerDroidContainerComponent.h"
@@ -262,7 +263,7 @@ ComponentManager::ComponentManager() {
 	components.put("CreatureHabitatMenuComponent", new CreatureHabitatMenuComponent());
 	components.put("WearableObjectMenuComponent", new WearableObjectMenuComponent());
 	components.put("ArmorObjectMenuComponent", new ArmorObjectMenuComponent());
-
+	components.put("ArmorObjectMenuComponent1", new ArmorObjectMenuComponent1());
 	components.put("CityHallZoneComponent", new CityHallZoneComponent());
 
 	components.put("LightsaberObjectMenuComponent", new LightsaberObjectMenuComponent());
